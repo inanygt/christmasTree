e2b32f9c84517eae5ff5dbfb0b8729c91007c97c
Inan Yigit <inanygt@hotmail.com>
2022-12-06 11:08:27 +0100

    init project

---
+++ script.js
@@ -0,0 +1,16 @@
+// let days = document.getElementById("days");
+// days.innerHTML = "test";
+
+const myDate = new Date();
+const xmas = Date.parse("Dec 25, " + myDate.getFullYear());
+const today = Date.parse(myDate);
+
+const daysToChristmas = Math.round((xmas - today) / (1000 * 60 * 60 * 24));
+
+if (daysToChristmas == 0) $("#days").text("It's Christmas!! Merry Christmas!");
+
+if (daysToChristmas < 0)
+   $("#days").text("Christmas was " + -1 * daysToChristmas + " days ago.");
+
+if (daysToChristmas > 0)
+   $("#days").text(daysToChristmas + " days to Christmas!");
